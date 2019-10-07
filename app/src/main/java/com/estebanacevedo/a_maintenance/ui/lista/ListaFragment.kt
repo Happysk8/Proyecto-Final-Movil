04ff3b498cc5ac19cc3c58707e4c71ccfeb532e0
Happysk8 <esteban.acevedo@udea.edu.co>
2019-10-07 17:21:30 -0500

    Fragmento Lista con recycler view... ItemViews estàticos, no abren.

--- app/src/main/java/com/estebanacevedo/a_maintenance/ui/lista/ListaFragment.kt
+++ app/src/main/java/com/estebanacevedo/a_maintenance/ui/lista/ListaFragment.kt
@@ -9,6 +9,10 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.estebanacevedo.a_maintenance.Activo
+import com.estebanacevedo.a_maintenance.ActivosAdapter
 import com.estebanacevedo.a_maintenance.R
 
 /**
@@ -17,6 +21,7 @@ import com.estebanacevedo.a_maintenance.R
 class ListaFragment : Fragment() {
 
     private lateinit var listaViewModel: ListaViewModel
+    private lateinit var recyclerView: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,11 +35,25 @@ class ListaFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 //        PageViewModel=
-        val root = inflater.inflate(R.layout.fragment_dashboard, container, false)
-        val textView: TextView = root.findViewById(R.id.txdashboard)
-        listaViewModel.text.observe(this, Observer<String> {
-            textView.text = it
-        })
+        val root = inflater.inflate(R.layout.fragment_lista, container, false)
+//        val textView: TextView = root.findViewById(R.id.txdashboard)
+//        listaViewModel.text.observe(this, Observer<String> {
+//            textView.text = it
+//        })
+        var activo:MutableList<Activo> = ArrayList()
+
+        activo.add(Activo("PTN1", "Pelton 1", "Turbina", "Porce1",R.drawable.pelton))
+        activo.add(Activo("PTN2", "Pelton 2", "Turbina", "Porce1",R.drawable.pelton))
+        activo.add(Activo("PTN3", "Pelton 3", "Turbina", "Porce1",R.drawable.pelton))
+        activo.add(Activo("FCS1", "Francis 1", "Turbina", "Porce1",R.drawable.francis))
+        activo.add(Activo("FCS2", "Francis 2", "Turbina", "Porce1",R.drawable.francis))
+        activo.add(Activo("FCS3", "Francis 3", "Turbina", "Porce1",R.drawable.francis))
+
+        recyclerView = root.findViewById(R.id.recycler) as RecyclerView
+        recyclerView.setHasFixedSize(true)
+        recyclerView.layoutManager = LinearLayoutManager(this.context, RecyclerView.VERTICAL,false)
+        val activosAdapter  = ActivosAdapter(activo!!,this.context)
+        recyclerView.adapter = activosAdapter
         return root
     }
 
